ea1fa24ed4f087bb65f9e094eec61b972342faf0
nalinter <62004370+nalinter@users.noreply.github.com>
2020-06-29 11:51:59 +0530

    Delete package_deployment.sh

--- shellscript/package_deployment.sh
+++
@@ -1,9 +0,0 @@
-echo $1
-echo $2
-if [ -d "prpackage_installer" ]; then
-	rm -r prpackage_installer
-fi
-git clone git@github.com:nalinter/prpackage_installer.git
-cd prpackage_installer
-python3 deploy_uninstall_pkg.py $1 $2
-python3 deploy_install_pkg.py $1 $2
