8ee9d16b6db4eff4b2758a39cdd706a95dce7962
nalinter <62004370+nalinter@users.noreply.github.com>
2020-06-29 11:49:35 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Slack Package Approval
+
+Install using:
+
+```shell script
+python3 -m pip install requirements.txt
+```
+
+Execute in the following format:
+
+```shell script
+python3 main.py <approval-url> <pr-number>
+```
---
+++ main.py
@@ -0,0 +1,122 @@
+import sys
+import json
+import requests
+
+
+if __name__ == '__main__':
+
+    webhook_url = 'https://hooks.slack.com/services/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'
+
+    pr_number = sys.argv[1]
+    pr_package_name = "module_" + pr_number + "_modules_1.zip"
+    console_output = sys.argv[2] + "console"
+    job_url = "http://ddywdcdevin01.sl.bluecloud.ibm.com:8080/job/sample-pipeline/PRE-SBX_Report"
+    data = {
+        "channel": "sbx-aproval",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Hi @sanojkvalappil @srutnair :wave: please ignore this message"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Environment: PRE-SBX"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*PR Number*: " + pr_number + "\n"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*PR Package Name*: " + pr_package_name + "\n"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Deployment To PRE-SBX: *Successful*\n "
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Smoke Test: *Successful*\n "
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Smoke Tests Report: " + job_url
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Click the below Button to go ahead and deploy package to SBX Environment"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": console_output
+                }
+            }
+            # {
+            #     "type": "section",
+            #     "text": {
+            #         "type": "mrkdwn",
+            #         "text": approval_url
+            #     }
+            # },
+            # {
+            #     "type": "actions",
+            #     "elements": [
+            #         {
+            #             "type": "button",
+            #             "text": {
+            #                 "type": "plain_text",
+            #                 "text": "Approve"
+            #             },
+            #             "value": "click_approve"
+            #         },
+            #         {
+            #             "type": "button",
+            #             "text": {
+            #                 "type": "plain_text",
+            #                 "text": "Reject"
+            #             },
+            #             "value": "click_reject"
+            #         }
+            #     ]
+            # }
+        ],
+        'username': 'HAL9000',
+        'icon_emoji': ':robot_face:'
+    }
+
+    response = requests.post(
+        webhook_url,
+        data=json.dumps(data),
+        headers={
+            'Content-Type': 'application/json'
+        }
+    )
+
+    print('Response: ' + str(response.text))
+    print('Response code: ' + str(response.status_code))
